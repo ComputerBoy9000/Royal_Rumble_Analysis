ac34c1e7725b35f38916dea04eb49f44313fe9aa
ComputerBoy9000 <ChristianFagan11@gmail.com>
2019-10-30 23:33:29 -0700

    Rough drafts and data cleaning

--- Rumble_Data_Cleaning.py
+++ Rumble_Data_Cleaning.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 RUMBLE_DATA = pd.read_csv('/home/christian/Desktop/Royal_Rumble_Analysis/RumbleData.csv').copy()
 
+
 def time_cleaner(time_string):
     """
     Convert video timestamps into seconds
@@ -13,9 +14,25 @@ RUMBLE_DATA['entry_time'] = RUMBLE_DATA['entry_time'].astype(str)
 RUMBLE_DATA['exit_time'] = RUMBLE_DATA['exit_time'].astype(str)
 ENTRY_TIME_SPLIT = RUMBLE_DATA['entry_time'].str.split(':')
 EXIT_TIME_SPLIT = RUMBLE_DATA['exit_time'].str.split(':')
-
 RUMBLE_DATA['entry_time'] = ENTRY_TIME_SPLIT.apply(time_cleaner)
 RUMBLE_DATA['exit_time'] = EXIT_TIME_SPLIT.apply(time_cleaner)
+
+RUMBLE_DATA['break_begin'] = RUMBLE_DATA['break_begin'].fillna(0)
+break_starts = RUMBLE_DATA.loc[RUMBLE_DATA['break_begin'] != 0, 'break_begin'] 
+break_starts = break_starts.astype(str).str.split(':').apply(time_cleaner)
+RUMBLE_DATA.loc[RUMBLE_DATA['break_begin'] != 0, 'break_begin'] = break_starts
+
+RUMBLE_DATA['break_end'] = RUMBLE_DATA['break_end'].fillna(0)
+break_ends = RUMBLE_DATA.loc[RUMBLE_DATA['break_end'] != 0, 'break_end'] 
+break_ends = break_ends.astype(str).str.split(':').apply(time_cleaner)
+RUMBLE_DATA.loc[RUMBLE_DATA['break_end'] != 0, 'break_end'] = break_ends
+
+RUMBLE_DATA['total_break'] = RUMBLE_DATA['break_end'] - RUMBLE_DATA['break_begin']
+
+RUMBLE_DATA['break'] = RUMBLE_DATA['break'].fillna(0)
+RUMBLE_DATA.loc[RUMBLE_DATA['total_break'] > 0, 'break'] = 1
+
+
 
 for year in RUMBLE_DATA['year'].unique():
     RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'exit_time'] = (RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'exit_time'] -
@@ -25,12 +42,12 @@ for year in RUMBLE_DATA['year'].unique():
                                                                   RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'entry_time'].min())
 
     
-RUMBLE_DATA['total_time'] = RUMBLE_DATA['exit_time'] - RUMBLE_DATA['entry_time']
+RUMBLE_DATA['total_time'] = RUMBLE_DATA['exit_time'] - RUMBLE_DATA['entry_time'] - RUMBLE_DATA['total_break']
 RUMBLE_DATA['total_elim'] = (RUMBLE_DATA['solo_elim'] + RUMBLE_DATA['group_elim'] + 
                              RUMBLE_DATA['illegal_elim'])
 
 RUMBLE_DATA['tag_partner'] = RUMBLE_DATA['tag_partner'].fillna(0)
-RUMBLE_DATA.fillna(0)
+
 MATCH_DATA = pd.DataFrame()
 
 state_dict = {}
@@ -43,16 +60,7 @@ for year in RUMBLE_DATA['year'].unique():
     split_times.sort()
     entry_times.sort()
     exit_times.sort()
-    for time in split_times:
-        state_vec = np.zeros(30)
-        for wrestler in RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'name']:
-            entry_number = int(RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) & (RUMBLE_DATA['name'] == wrestler), 'entry_num'])
-            time_in = int(RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) & (RUMBLE_DATA['name'] == wrestler), 'entry_time'])
-            time_out = int(RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) & (RUMBLE_DATA['name'] == wrestler), 'exit_time'])
-            if time_in <= time and time_out > time:
-                state_vec[entry_number - 1] = 1
-        year_state_dict[time] = state_vec
-    state_dict[year] = year_state_dict
+    
     year_list = []
     for i in range(len(split_times)):
         year_list.append(year)
@@ -87,17 +95,38 @@ for wrestler in RUMBLE_DATA['name'].unique():
         RUMBLE_DATA.loc[(RUMBLE_DATA['name'] == wrestler) &
                                 (RUMBLE_DATA['year']== year), 'avg_cum_time'] = time_count/year_count                       
 
-for year in MATCH_DATA['year'].unique():
-    rumble_states = state_dict[year]
-    i = 1
-    index = 0
-    splits = MATCH_DATA.loc[MATCH_DATA['year'] == year, 'split']
-    for time in splits[1:]:
-        while i < time:
-            rumble_states[i] = rumble_states[splits[index]]
-            num_in = (i,np.count_nonzero(rumble_states[splits[index]]))
-            i += 1
-        index +=1
+
+world_title_dict = eval(open('/home/christian/Desktop/Royal_Rumble_Analysis/world_title_dict','r').read())
+us_title_dict = eval(open('/home/christian/Desktop/Royal_Rumble_Analysis/US_title_dict','r').read())
+ic_title_dict = eval(open('/home/christian/Desktop/Royal_Rumble_Analysis/IC_Title_dict').read())
+
+RUMBLE_DATA['world_reigns'] = RUMBLE_DATA['world_reigns'].fillna(0)
+RUMBLE_DATA['us_reigns'] = RUMBLE_DATA['us_reigns'].fillna(0)
+RUMBLE_DATA['ic_reigns' ]= RUMBLE_DATA['ic_reigns'].fillna(0)
+
+for year in world_title_dict.keys():
+    for wrestler in list(world_title_dict[year].keys()):
+        if wrestler in RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'name'].tolist():
+            RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) 
+                                & (RUMBLE_DATA['name'] == wrestler), 'world_reigns'] = world_title_dict[year][wrestler]
+
+    
+for year in us_title_dict.keys():
+    for wrestler in list(us_title_dict[year].keys()):
+        if wrestler in RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'name'].tolist():
+            RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) 
+                                & (RUMBLE_DATA['name'] == wrestler), 'us_reigns'] = us_title_dict[year][wrestler]
+
+for year in ic_title_dict.keys():
+    for wrestler in list(ic_title_dict[year].keys()):
+        if wrestler in RUMBLE_DATA.loc[RUMBLE_DATA['year'] == year, 'name'].tolist():
+            RUMBLE_DATA.loc[(RUMBLE_DATA['year'] == year) 
+                                & (RUMBLE_DATA['name'] == wrestler), 'ic_reigns'] = ic_title_dict[year][wrestler] 
+
+
+RUMBLE_DATA = RUMBLE_DATA.fillna(0)
 
 RUMBLE_DATA.to_csv('/home/christian/Desktop/Royal_Rumble_Analysis/Rumble_Data_Cleaned.csv')
 MATCH_DATA.to_csv('/home/christian/Desktop/Royal_Rumble_Analysis/Match_Data.csv')
+
+
